599cfcb632be185faa9f0124ee2588d95eeef0fd
Ryan Borja <ryan.t.bortja@gmail.com>
2018-07-09 11:03:01 -0400

    freshest fruit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,15 +12,17 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
+    <link href="{{ asset('css/layout.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <!-- Branding Image -->
-            <a class="navbar-brand" href="{{ url('/') }}">
+  
+        <!-- Branding Image -->
+        <a class="navbar-brand" href="{{ url('/') }}" style="float:left;">
                     <img src="{{ asset('logo/qsc_ball.png') }}" class="img-thumbnail" style="height: 200px">
-            </a>
+        </a>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            
             <!-- Collapsed Hamburger -->
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#app-navbar-collapse" aria-controls="app-navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -33,16 +35,22 @@
                         <a class="nav-link {{Request::is('/') ? 'active' : ''}}" href="/">Home <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link {{Request::is('contact') ? 'active' : ''}}" href="{{ url('/contact') }}">Contact</a>
+                        <a class="nav-link {{Request::is('about') ? 'active' : ''}}" href="{{ url('/about') }}">About</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link {{Request::is('portfolio') ? 'active' : ''}}" href="/portfolio">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link {{Request::is('materials') ? 'active' : ''}}" href="{{ url('/materials') }}">Materials</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link {{Request::is('contact') ? 'active' : ''}}" href="{{ url('/contact') }}">Contact Us</a>
                     </li>
                 </ul>
   
             </div>
         </nav>
-    
+       
     @yield('content')
    
     </div>
