9acad05d6a0b3f1031866308198264feea7dfa93
Ryan Borja <ryan.t.bortja@gmail.com>
2018-08-08 13:15:24 -0400

    spell check and new landing image

--- resources/views/public pages/about.blade.php
+++ resources/views/public pages/about.blade.php
@@ -16,7 +16,7 @@
             @endslot
             @slot('cardBody')
                 <p class="card-text p-3 mx-auto" style="width: 70%;">Viktor Grishcuk came to the United States from the Ukraine in 1994 and began granite fabrication in Seattle Washington in 2000. In 2003 Viktor moved to Kentucky and began installation and fabrication for several local fabricators. In November 2005 Quality Stone Countertops was born with Viktor and Alex Glushchak, along with two other fabricators.</p>
-                <p class="card-text p-3 mx-auto" style="width: 70%;">Today, we are growing and expanding over the entire state of Kentucky. With Viktor and Alex's reputation for craftmandship and service word has spread quickly. We aquire most of our business from word of mouth and referrals in the industry.</p>
+                <p class="card-text p-3 mx-auto" style="width: 70%;">Today, we are growing and expanding over the entire state of Kentucky. With Viktor and Alex's reputation for craftmanship and service word has spread quickly. We acquire most of our business from word of mouth and referrals in the industry.</p>
             @endslot
             @slot('cardImg')
                 <img class="card-img-bottom heightfix" src="{{ asset('pictures/history/owners.jpg') }}" alt="the owners">
